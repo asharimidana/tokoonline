d1e5ab3bf6efde3d5a887bfb0a04859cf88f19bb
asharimidana <asharimidana1@gmail.com>
2023-01-18 14:19:45 +0800

    o

--- frontend/src/Pages/Checkout.js
+++ frontend/src/Pages/Checkout.js
@@ -1,13 +1,13 @@
-import  NavbarApp  from "../components/NavbarApp2";
-import  Footer from "../components/Footer";
-import  ConfimCheckout from "../components/COnfirmCheckout";
+import NavbarApp from "../components/NavbarApp2";
+import Footer from "../components/Footer";
+import ConfimCheckout from "../components/COnfirmCheckout";
 function Checkout() {
-	return (
-		<div>
-		{<NavbarApp />}
-		{<ConfimCheckout/>}
-		{<Footer/>}
-		</div>
-	)
+  return (
+    <div>
+      {<NavbarApp />}
+      {<ConfimCheckout />}
+      {<Footer />}
+    </div>
+  );
 }
 export default Checkout;
--- frontend/src/components/ListProductAll.js
+++ frontend/src/components/ListProductAll.js
@@ -1,133 +1,191 @@
 import { Link } from "react-router-dom";
 
 function ListProductAll() {
-	return (
-		<div className="container " >
-			{/* row product list 1*/}
-			<div className="row py-2">
+  return (
+    <div className="container ">
+      {/* row product list 1*/}
+      <div className="row py-2">
+        <div className="col-lg-2 m-0 p-1">
+          <div className="card card-product-grid">
+            <Link className="img-wrap">
+              <>
+                <img
+                  className="w-100"
+                  alt="product"
+                  src={
+                    "https://ae01.alicdn.com/kf/Sdcb45e5c40e64c0e837214bacd02b65fB/disney-Marvel-Avengers-Groot-Mini-figure-Pen-Holder-Flower-Pot-Anime-Toys-Action-Figure-Sitting-Groot.jpg_220x220xz.jpg_.webp"
+                  }
+                />
+              </>
+            </Link>
+            <div className="info-wrap border-top px-2">
+              <div className="price fw-bold text-app">Rp29.98</div>
+              <div>
+                <small className="m-0 text-muted">
+                  200 terjual &#9733; 4.5
+                </small>
+              </div>
+              <Link className="title m-0 text-decoration-none fw-bold text-muted">
+                Mens T-shirt Cotton Base
+              </Link>
+              <br />
+              <br />
+              <small className="text-muted">jakarta</small>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-2 m-0 p-1">
+          <div className="card card-product-grid">
+            <Link className="img-wrap">
+              <>
+                <img
+                  className="w-100"
+                  alt="product"
+                  src={
+                    "https://ae01.alicdn.com/kf/Sa1f2e5b5134e4d15ac74a26af5f272a50/Jam-Tangan-Kuarsa-Bisnis-Pria-Jam-Tangan-Pria-Kalender-Enam-Mata-Tiga-Mata-Palsu-Modis-Jam.jpg_Q90.jpg_.webp"
+                  }
+                />
+              </>
+            </Link>
+            <div className="info-wrap border-top px-2">
+              <div className="price fw-bold text-app">Rp29.98</div>
+              <div>
+                <small className="m-0 text-muted">
+                  200 terjual &#9733; 4.5
+                </small>
+              </div>
+              <Link className="title m-0 text-decoration-none fw-bold text-muted">
+                Mens T-shirt Cotton Base
+              </Link>
+              <br />
+              <br />
+              <small className="text-muted">jakarta</small>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-2 m-0 p-1">
+          <div className="card card-product-grid">
+            <Link className="img-wrap">
+              <>
+                <img
+                  className="w-100"
+                  alt="product"
+                  src={
+                    "https://ae01.alicdn.com/kf/Se82e65e3441245d0b23b2b3211d55e41C/Germany-nail-clipper-set-a-full-set-of-new-high-grade-men-and-women-on-special.jpg_220x220xz.jpg_.webp"
+                  }
+                />
+              </>
+            </Link>
+            <div className="info-wrap border-top px-2">
+              <div className="price fw-bold text-app">Rp29.98</div>
+              <div>
+                <small className="m-0 text-muted">
+                  200 terjual &#9733; 4.5
+                </small>
+              </div>
+              <Link className="title m-0 text-decoration-none fw-bold text-muted">
+                Mens T-shirt Cotton Base
+              </Link>
+              <br />
+              <br />
+              <small className="text-muted">jakarta</small>
+            </div>
+          </div>
+        </div>
 
-				<div className="col-lg-2 m-0 p-1">
-					<div className="card card-product-grid">
-						<Link className="img-wrap">
-							<>
-								<img className="w-100" alt="product" src={"https://ae01.alicdn.com/kf/Sdcb45e5c40e64c0e837214bacd02b65fB/disney-Marvel-Avengers-Groot-Mini-figure-Pen-Holder-Flower-Pot-Anime-Toys-Action-Figure-Sitting-Groot.jpg_220x220xz.jpg_.webp"} />
-							</>
-						</Link>
-						<div className="info-wrap border-top px-2">
-							<div className="price fw-bold text-app">Rp29.98</div>
-							<div>
-								<small className="m-0 text-muted">200 terjual &#9733; 4.5</small>
-							</div>
-							<Link className="title m-0 text-decoration-none fw-bold text-muted">Mens T-shirt Cotton Base</Link>
-							<br />
-							<br />
-							<small className="text-muted">jakarta</small>
-						</div>
-					</div >
-				</div>
-				<div className="col-lg-2 m-0 p-1">
-					<div className="card card-product-grid">
-						<Link className="img-wrap">
-							<>
-								<img className="w-100" alt="product" src={"https://ae01.alicdn.com/kf/Sa1f2e5b5134e4d15ac74a26af5f272a50/Jam-Tangan-Kuarsa-Bisnis-Pria-Jam-Tangan-Pria-Kalender-Enam-Mata-Tiga-Mata-Palsu-Modis-Jam.jpg_Q90.jpg_.webp"} />
-							</>
-						</Link>
-						<div className="info-wrap border-top px-2">
-							<div className="price fw-bold text-app">Rp29.98</div>
-							<div>
-								<small className="m-0 text-muted">200 terjual &#9733; 4.5</small>
-							</div>
-							<Link className="title m-0 text-decoration-none fw-bold text-muted">Mens T-shirt Cotton Base</Link>
-							<br />
-							<br />
-							<small className="text-muted">jakarta</small>
-						</div>
-					</div >
-				</div>
-				<div className="col-lg-2 m-0 p-1">
-					<div className="card card-product-grid">
-						<Link className="img-wrap">
-							<>
-								<img className="w-100" alt="product" src={"https://ae01.alicdn.com/kf/Se82e65e3441245d0b23b2b3211d55e41C/Germany-nail-clipper-set-a-full-set-of-new-high-grade-men-and-women-on-special.jpg_220x220xz.jpg_.webp"} />
-							</>
-						</Link>
-						<div className="info-wrap border-top px-2">
-							<div className="price fw-bold text-app">Rp29.98</div>
-							<div>
-								<small className="m-0 text-muted">200 terjual &#9733; 4.5</small>
-							</div>
-							<Link className="title m-0 text-decoration-none fw-bold text-muted">Mens T-shirt Cotton Base</Link>
-							<br />
-							<br />
-							<small className="text-muted">jakarta</small>
-						</div>
-					</div >
-				</div>
+        <div className="col-lg-2 m-0 p-1">
+          <div className="card card-product-grid">
+            <Link className="img-wrap">
+              <>
+                <img
+                  className="w-100"
+                  alt="product"
+                  src={
+                    "https://ae01.alicdn.com/kf/Sb3cbdc26a92941e19040a95a0b1b6bf0K/Luxury-Matte-Magnetic-For-Magsafe-Wireless-Charge-Case-For-iPhone-14-13-11-12-Pro-Max.jpg_220x220xz.jpg_.webp"
+                  }
+                />
+              </>
+            </Link>
+            <div className="info-wrap border-top px-2">
+              <div className="price fw-bold text-app">Rp29.98</div>
+              <div>
+                <small className="m-0 text-muted">
+                  200 terjual &#9733; 4.5
+                </small>
+              </div>
+              <Link className="title m-0 text-decoration-none fw-bold text-muted">
+                Mens T-shirt Cotton Base
+              </Link>
+              <br />
+              <br />
+              <small className="text-muted">jakarta</small>
+            </div>
+          </div>
+        </div>
 
-				<div className="col-lg-2 m-0 p-1">
-					<div className="card card-product-grid">
-						<Link className="img-wrap">
-							<>
-								<img className="w-100" alt="product" src={"https://ae01.alicdn.com/kf/Sb3cbdc26a92941e19040a95a0b1b6bf0K/Luxury-Matte-Magnetic-For-Magsafe-Wireless-Charge-Case-For-iPhone-14-13-11-12-Pro-Max.jpg_220x220xz.jpg_.webp"} />
-							</>
-						</Link>
-						<div className="info-wrap border-top px-2">
-							<div className="price fw-bold text-app">Rp29.98</div>
-							<div>
-								<small className="m-0 text-muted">200 terjual &#9733; 4.5</small>
-							</div>
-							<Link className="title m-0 text-decoration-none fw-bold text-muted">Mens T-shirt Cotton Base</Link>
-							<br />
-							<br />
-							<small className="text-muted">jakarta</small>
-						</div>
-					</div >
-				</div>
+        <div className="col-lg-2 m-0 p-1">
+          <div className="card card-product-grid">
+            <Link className="img-wrap">
+              <>
+                <img
+                  className="w-100"
+                  alt="product"
+                  src={
+                    "https://ae01.alicdn.com/kf/S375262439133452aa3f46de6582bcc14f/Silicone-Strap-For-Apple-Watch-Band-44mm-40mm-45mm-41mm-49mm-42mm-38mm-44-45-mm.jpg_220x220xz.jpg_.webp"
+                  }
+                />
+              </>
+            </Link>
+            <div className="info-wrap border-top px-2">
+              <div className="price fw-bold text-app">Rp29.98</div>
+              <div>
+                <small className="m-0 text-muted">
+                  200 terjual &#9733; 4.5
+                </small>
+              </div>
+              <Link className="title m-0 text-decoration-none fw-bold text-muted">
+                Mens T-shirt Cotton Base
+              </Link>
+              <br />
+              <br />
+              <small className="text-muted">jakarta</small>
+            </div>
+          </div>
+        </div>
 
-				<div className="col-lg-2 m-0 p-1">
-					<div className="card card-product-grid">
-						<Link className="img-wrap">
-							<>
-								<img className="w-100" alt="product" src={"https://ae01.alicdn.com/kf/S375262439133452aa3f46de6582bcc14f/Silicone-Strap-For-Apple-Watch-Band-44mm-40mm-45mm-41mm-49mm-42mm-38mm-44-45-mm.jpg_220x220xz.jpg_.webp"} />
-							</>
-						</Link>
-						<div className="info-wrap border-top px-2">
-							<div className="price fw-bold text-app">Rp29.98</div>
-							<div>
-								<small className="m-0 text-muted">200 terjual &#9733; 4.5</small>
-							</div>
-							<Link className="title m-0 text-decoration-none fw-bold text-muted">Mens T-shirt Cotton Base</Link>
-							<br />
-							<br />
-							<small className="text-muted">jakarta</small>
-						</div>
-					</div >
-				</div>
-
-				<div className="col-lg-2 m-0 p-1">
-					<div className="card card-product-grid">
-						<Link className="img-wrap">
-							<>
-								<img className="w-100 rounded" alt="product" src={"https://ae01.alicdn.com/kf/S375262439133452aa3f46de6582bcc14f/Silicone-Strap-For-Apple-Watch-Band-44mm-40mm-45mm-41mm-49mm-42mm-38mm-44-45-mm.jpg_220x220xz.jpg_.webp"} />
-							</>
-						</Link>
-						<div className="info-wrap border-top px-2">
-							<div className="price fw-bold text-app">Rp29.98</div>
-							<div>
-								<small className="m-0 text-muted">200 terjual &#9733; 4.5</small>
-							</div>
-							<Link className="title m-0 text-decoration-none fw-bold text-muted">Mens T-shirt Cotton Base</Link>
-							<br />
-							<br />
-							<small className="text-muted">jakarta</small>
-						</div>
-					</div >
-				</div>
-				{/* end row product list 1*/}
-
-			</div>
-		</div>
-	);
+        <div className="col-lg-2 m-0 p-1">
+          <div className="card card-product-grid">
+            <Link className="img-wrap">
+              <>
+                <img
+                  className="w-100 rounded"
+                  alt="product"
+                  src={
+                    "https://ae01.alicdn.com/kf/S375262439133452aa3f46de6582bcc14f/Silicone-Strap-For-Apple-Watch-Band-44mm-40mm-45mm-41mm-49mm-42mm-38mm-44-45-mm.jpg_220x220xz.jpg_.webp"
+                  }
+                />
+              </>
+            </Link>
+            <div className="info-wrap border-top px-2">
+              <div className="price fw-bold text-app">Rp29.98</div>
+              <div>
+                <small className="m-0 text-muted">
+                  200 terjual &#9733; 4.5
+                </small>
+              </div>
+              <Link className="title m-0 text-decoration-none fw-bold text-muted">
+                Mens T-shirt Cotton Base
+              </Link>
+              <br />
+              <br />
+              <small className="text-muted">jakarta</small>
+            </div>
+          </div>
+        </div>
+        {/* end row product list 1*/}
+      </div>
+    </div>
+  );
 }
 
 export default ListProductAll;
